e2790e609d68b4aebd4c13f6401e70a495d6f31b
jackliu1990 <1021563889@qq.com>
2017-08-11 16:58:55 +0800

    提交坐标修改

--- src/main/java/com/gs/gis/geometry/Coordinate.java
+++ src/main/java/com/gs/gis/geometry/Coordinate.java
@@ -4,5 +4,12 @@ public class Coordinate {
 	private double x;
 	private double y;
 	private double z = Double.NaN;
+	private double m = Double.NaN;
+	private boolean is3D = false;
+	private boolean isMeasured = false;
 	
+	
+	public static final int X = 0;
+	public static final int Y = 1;
+	public static final int Z = 2;	
 }
