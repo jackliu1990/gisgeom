2c67efc60486177cb64ddbb9f71b8ee7dbb3eef4
jackliu1990 <1021563889@qq.com>
2017-08-22 14:05:09 +0800

    去掉加入的序列化操作

--- src/main/java/com/gs/gis/geometry/Geometry.java
+++ src/main/java/com/gs/gis/geometry/Geometry.java
@@ -1,7 +1,6 @@
 package com.gs.gis.geometry;
 
 import java.io.Serializable;
-import com.fasterxml.jackson.annotation.*;
 
 public abstract class Geometry implements Serializable {
 	private static final long serialVersionUID = 5594065733859514709L;
@@ -22,32 +21,24 @@ public abstract class Geometry implements Serializable {
 		this.srid = srid;
 	}
 	
-	@JsonIgnore
 	public abstract String getGeometryType();
 	
-	@JsonIgnore
 	public abstract Geometry getEnvelope();
 	
-	@JsonIgnore
 	public abstract int getDimension();
 	
-	@JsonIgnore
 	public abstract boolean isEmpty();
 
-	@JsonIgnore
 	public abstract boolean is3D();
 
-	@JsonIgnore
 	public abstract boolean isMeasured();
 	
 	public abstract Coordinate getCoordinate();
 	
 	public abstract Coordinate[] getCoordinates();
 	
-	@JsonIgnore
 	public abstract String asText();
 	
-	@JsonIgnore
 	public abstract byte[] asBinary();
 	
 }
